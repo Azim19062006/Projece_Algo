eb1926461b3cfa86e94b2bbc1a073ed3ec473c0f
Almaz192 <83873332+Almaz192@users.noreply.github.com>
2024-06-05 19:43:42 +0600

    Update Program.cs
    libarries and Node class

--- Projece_Algo/Program.cs
+++ Projece_Algo/Program.cs
@@ -1,1 +1,14 @@
-﻿
+using System;
+using System.Text;
+
+class Node
+{
+    public Note Data { get; set; }
+    public Node Next { get; set; }
+
+    public Node(Note data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
