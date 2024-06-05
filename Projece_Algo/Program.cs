c778f8a93e74563f52afd7bc6f8ed6ccabbf8fb0
Azim19062006 <mamatbaevazim06@gmail.com>
2024-06-05 19:50:02 +0600

    Update Program.cs
    Description

--- Projece_Algo/Program.cs
+++ Projece_Algo/Program.cs
@@ -12,3 +12,89 @@ class Node
         Next = null;
     }
 }
+class LinkedList
+{
+    private Node head;
+
+    public LinkedList()
+    {
+        head = null;
+    }
+
+    public void Add(Note data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public bool Remove(string title)
+    {
+        if (head == null)
+        {
+            return false;
+        }
+
+        if (head.Data.Title.Equals(title))
+        {
+            head = head.Next;
+            return true;
+        }
+
+        Node current = head;
+        while (current.Next != null && !current.Next.Data.Title.Equals(title))
+        {
+            current = current.Next;
+        }
+
+        if (current.Next == null)
+        {
+            return false;
+        }
+
+        current.Next = current.Next.Next;
+        return true;
+    }
+
+    public bool Edit(string title, string newContent)
+    {
+        Node current = head;
+        while (current != null)
+        {
+            if (current.Data.Title.Equals(title))
+            {
+                current.Data.Content = newContent;
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void Display()
+    {
+        Node current = head;
+        int count = 1;
+        while (current != null)
+        {
+            Console.WriteLine($"{count++}) {current.Data}");
+            current = current.Next;
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
+}
